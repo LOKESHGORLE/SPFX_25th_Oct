c09ae3dae17604458f87635b65d69aa561a2c9f1
LOKESHGORLE <43067941+LOKESHGORLE@users.noreply.github.com>
2018-10-28 04:37:29 +0530

    completed all the data part and UI
    pie Chart To be done

--- PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
+++ PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
@@ -4,8 +4,10 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { escape, isEmpty } from '@microsoft/sp-lodash-subset';
 
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
+import pnp from 'sp-pnp-js';
 import { SPComponentLoader } from '@microsoft/sp-loader';
 //import  * as $ from 'jquery';
 import 'jquery';
@@ -51,17 +53,70 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
 
      `;
+     this.getReady();
+  /*-------------------to get the id of the user ///// failed ////--------  */  
+var a;
+var b;
+      
+      pnp.sp.web.lists.getByTitle('LokPollingVenues').items.select('Title, ID').filter(`Title eq 'London'`)
+      .get() .then((items: any[]) => {
+        console.log(items);
+    });
+  //.then(response => {
+  //   console.log(response.map(field => {
+  //     return {
+  //       Title: field.Title,
+	// ID: field.ID
+  //     };
+  //   }));
+  //})
 
-      this.getReady();
   }
-
+/*------------------ calling the document ready and all the jquery-------------    */
   private getReady(){
     var ParentSiteUrl = this.context.pageContext.web.absoluteUrl;
+    var SelectedBtnID;
+    var CurrUseremail=this.context.pageContext.user.email;
+    var CurrUserIdInList;
+         //alert(CurrUseremail);
 
     $(document).ready(function(){
-      alert("enetred jquery");
+      alert("enetred jquery");  
       
-     
+      /**------ verifying User in the list ------------------ */
+
+      var callAssignDisplayItems = jQuery.ajax({
+        url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingMembers')/items?$select=MemberName,ID,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID",
+        type: "GET",
+        dataType: "json",
+        headers: {
+          Accept: "application/json;odata=verbose"
+        }
+      });
+
+    var call = jQuery.when(callAssignDisplayItems);
+      call.done(function (data, textStatus, jqXHR) {
+        
+        jQuery.each(data.d.results, function (index, value) {
+
+         //alert(value.MemberName+"     "+value.PlaceOfInterest.ID);
+         /**---------- verifying if the user is present in the list or not------------- */
+          if(value.MemberName==CurrUseremail){
+            CurrUserIdInList=value.ID;
+            SelectedBtnID=value.PlaceOfInterest.ID;
+            CreateVenueButtons(SelectedBtnID);//calling button creation function
+          }
+
+      });
+      });
+      call.fail(function (jqXHR, textStatus, errorThrown) {
+        var response = JSON.parse(jqXHR.responseText);
+        var message = response ? response.error.message.value : textStatus;
+        alert("Call failed. Error: " + message);
+      });
+      
+      /**------------ creating HTML element to show VENUE---------------------- */
+      function CreateVenueButtons(SelectedBtnID){
           var callAssignDisplayItems = jQuery.ajax({
               url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description",
               type: "GET",
@@ -75,26 +130,84 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
             call.done(function (data, textStatus, jqXHR) {
               var  PollTabbody= jQuery("#PollTabBody");
             var VoteItemCreation;
-            var ItemClass= "btn btn-primary VotePollbut";
+            var ItemClass= "";
               jQuery.each(data.d.results, function (index, value) {
 
-                //  if(index=='0'){ItemClass="item active"}else{ItemClass="item"};
-          
-          VoteItemCreation= "<tr><td>"+value.Description+"</td><td><button type='button' id='"+value.ID+"'class='"+ItemClass+"'>"+value.Title+"</button></td></tr>";    
+                  //if(value.ID==SelectedBtnID){ItemClass="active"}else{ItemClass=""};
+          /**-------------------------creating HTML TAG----------------------------- */
+          VoteItemCreation= "<tr><td>"+value.Description+"</td><td><button type='button' id='"+value.ID+"'class='btn btn-primary VotePollbut "+ItemClass+"'>"+value.Title+"</button></td></tr>";    
            
            PollTabbody.append(VoteItemCreation);
   
             });
             });
+           
   
           call.fail(function (jqXHR, textStatus, errorThrown) {
               var response = JSON.parse(jqXHR.responseText);
               var message = response ? response.error.message.value : textStatus;
               alert("Call failed. Error: " + message);
             });
+          }/**--------- ends the HTML creation for Venues  --------------------*/
+         
+          
     });
+    $(document).on("click", ".btn-primary" , function() {
+      SelectedBtnID=0;
+      var clicked = $(this);
+      if(clicked.hasClass('active')){
+        $('.VotePollbut').prop('disabled', false);
+      clicked.removeClass('active');
+      }else{
+        $('.VotePollbut').prop('disabled', true);
+        clicked.prop('disabled', false).addClass("active");
+      }
+      
+
+      
+    });
+
+    
+   
+    /**------- Submit Vote clicked,.. list has to be updated------------- */
+    $(document).on("click", "#VoteSubmit" , function() {
+      UpdateItem(SelectedBtnID);
+    });
+    function UpdateItem(SelectedBtnID)
+    { 
+      $('#PollTabBody .active').each(function(){
+        /**-------------------------Id Of the active button iscollected------------  */
+        SelectedBtnID= $(this).attr('id'); 
+        if(isEmpty(SelectedBtnID) ){
+          alert("12")
+        }{
+          alert(SelectedBtnID);
+        }
+        
+        
+    });
+      /**----------- list updated----------------- */
+        pnp.sp.web.lists.getByTitle("LokPollingMembers").items.getById(CurrUserIdInList).update({
+         PlaceOfInterestId : SelectedBtnID   });
+        
+    }
+   
   }
  
+  
+ 
+  /* ------------ getting user details
+  alert(email);
+public getUserId(email: string): Promise<number> {
+return pnp.sp.site.rootWeb.ensureUser(email).then(result => {
+return result.data.Id;
+});
+} */
+
+  
+  
+
+
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
