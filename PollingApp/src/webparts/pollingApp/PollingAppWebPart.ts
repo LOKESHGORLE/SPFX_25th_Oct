f68ae19f763bc467903d6b7cff88c6788565087f
LOKESHGORLE <43067941+LOKESHGORLE@users.noreply.github.com>
2018-10-28 16:38:41 +0530

    sample pie chart updated
    yet to do with list data

--- PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
+++ PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
@@ -13,7 +13,7 @@ import { SPComponentLoader } from '@microsoft/sp-loader';
 import 'jquery';
 require('bootstrap');
 //import  'charts';
-
+import Chart from 'chart.js';
 import styles from './PollingAppWebPart.module.scss';
 import * as strings from 'PollingAppWebPartStrings';
 
@@ -46,7 +46,9 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
         <button type='button center-block' id='VoteSubmit'class='VoteSubmitClass'>Submit Vote</button>
       </div>
   </div>
-  <div id="PieChart"></div>
+  <div id="PieChart">
+    <canvas id="pie-chart" width="30%" height="30%"></canvas>
+  </div>
   
 
 
@@ -55,36 +57,56 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
 
      `;
-     this.getReady();
-  /*-------------------to get the id of the user ///// failed ////--------  */  
-var a;
-var b;
-      
-      pnp.sp.web.lists.getByTitle('LokPollingVenues').items.select('Title, ID').filter(`Title eq 'London'`)
-      .get() .then((items: any[]) => {
+    this.getReady();
+    /*-------------------to get the id of the user ///// failed ////--------  */
+    var a;
+    var b;
+
+    pnp.sp.web.lists.getByTitle('LokPollingVenues').items.select('Title, ID').filter(`Title eq 'London'`)
+      .get().then((items: any[]) => {
         console.log(items);
-    });
-  //.then(response => {
-  //   console.log(response.map(field => {
-  //     return {
-  //       Title: field.Title,
-	// ID: field.ID
-  //     };
-  //   }));
-  //})
+      });
+    //.then(response => {
+    //   console.log(response.map(field => {
+    //     return {
+    //       Title: field.Title,
+    // ID: field.ID
+    //     };
+    //   }));
+    //})
 
   }
-/*------------------ calling the document ready and all the jquery-------------    */
-  private getReady(){
+  /*------------------ calling the document ready and all the jquery-------------    */
+  private getReady() {
     var ParentSiteUrl = this.context.pageContext.web.absoluteUrl;
     var SelectedBtnID;
-    var CurrUseremail=this.context.pageContext.user.email;
+    var CurrUseremail = this.context.pageContext.user.email;
     var CurrUserIdInList;
-         //alert(CurrUseremail);
+    //alert(CurrUseremail);
 
-    $(document).ready(function(){
-      alert("enetred jquery");  
-      
+    $(document).ready(function () {
+      /************************Pie Chart*****************/
+      new Chart(document.getElementById("pie-chart"), {
+        type: 'pie',
+        data: {
+          labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+          datasets: [{
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: [2478,5267,734,784,433]
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
+          }
+        }
+    });
+
+ /************************Pie Chart*****************/
+      alert("enetred jquery");
+
       /**------ verifying User in the list ------------------ */
 
       var callAssignDisplayItems = jQuery.ajax({
@@ -96,145 +118,146 @@ var b;
         }
       });
 
-    var call = jQuery.when(callAssignDisplayItems);
+      var call = jQuery.when(callAssignDisplayItems);
       call.done(function (data, textStatus, jqXHR) {
-        
+
         jQuery.each(data.d.results, function (index, value) {
 
-         //alert(value.MemberName+"     "+value.PlaceOfInterest.ID);
-         /**---------- verifying if the user is present in the list or not------------- */
-          if(value.MemberName==CurrUseremail){
-            CurrUserIdInList=value.ID;
-            SelectedBtnID=value.PlaceOfInterest.ID;
-            
+          //alert(value.MemberName+"     "+value.PlaceOfInterest.ID);
+          /**---------- verifying if the user is present in the list or not------------- */
+          if (value.MemberName == CurrUseremail) {
+            CurrUserIdInList = value.ID;
+            SelectedBtnID = value.PlaceOfInterest.ID;
+
             CreateVenueButtons(SelectedBtnID);//calling button creation function
           }
 
-      });
+        });
       });
       call.fail(function (jqXHR, textStatus, errorThrown) {
         var response = JSON.parse(jqXHR.responseText);
         var message = response ? response.error.message.value : textStatus;
         alert("Call failed. Error: " + message);
       });
-      
-      /**------------ creating HTML element to show VENUE---------------------- */
-      function CreateVenueButtons(SelectedBtnID){
-          var callAssignDisplayItems = jQuery.ajax({
-              url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description",
-              type: "GET",
-              dataType: "json",
-              headers: {
-                Accept: "application/json;odata=verbose"
-              }
-            });
-  
-          var call = jQuery.when(callAssignDisplayItems);
-            call.done(function (data, textStatus, jqXHR) {
-              var  PollTabbody= jQuery("#PollTabBody");
-            var VoteItemCreation;
-            var ItemClass= "";
-              jQuery.each(data.d.results, function (index, value) {
 
-                  //if(value.ID==SelectedBtnID){ItemClass="active"}else{ItemClass=""};
-          /**-------------------------creating HTML TAG----------------------------- */
-          VoteItemCreation= "<tr><td>"+value.Description+"</td><td><button type='button' id='"+value.ID+"'class='btn btn-primary VotePollbut "+ItemClass+"'>"+value.Title+"</button></td></tr>";    
-           
-           PollTabbody.append(VoteItemCreation);
-  
-            });
-            if(true){
-              alert("true");
-              alert(SelectedBtnID);
-              $('.VotePollbut').prop('disabled', true);
-              $("#"+SelectedBtnID).addClass("active").prop('disabled', false);
-            }
-            });
-           
-  
-          call.fail(function (jqXHR, textStatus, errorThrown) {
-              var response = JSON.parse(jqXHR.responseText);
-              var message = response ? response.error.message.value : textStatus;
-              alert("Call failed. Error: " + message);
-            });
-            
-            
+      /**------------ creating HTML element to show VENUE---------------------- */
+      function CreateVenueButtons(SelectedBtnID) {
+        var callAssignDisplayItems = jQuery.ajax({
+          url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description",
+          type: "GET",
+          dataType: "json",
+          headers: {
+            Accept: "application/json;odata=verbose"
           }
-          
-          
-          /**--------- ends the HTML creation for Venues  --------------------*/
-         
-          /**------- Chart Creation------------------------------------------------------ */
-          //ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingMembers')/items?$select=MemberName,ID,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID"
-         /* var dataURL = ParentSiteUrl + "/_api/Lists/getbyTitle('LokPollingMembers')/Items?$top=50&$select=Title,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID";
-          $.getJSON(dataURL, function (data) {
-              var dataFromSharepointList = data.value;
-  
-              drawChart(dataFromSharepointList, "PieChart")
+        });
+
+        var call = jQuery.when(callAssignDisplayItems);
+        call.done(function (data, textStatus, jqXHR) {
+          var PollTabbody = jQuery("#PollTabBody");
+          var VoteItemCreation;
+          var ItemClass = "";
+          jQuery.each(data.d.results, function (index, value) {
+
+
+            /**-------------------------creating HTML TAG----------------------------- */
+            VoteItemCreation = "<tr><td>" + value.Description + "</td><td><button type='button' id='" + value.ID + "'class='btn btn-primary VotePollbut " + ItemClass + "'>" + value.Title + "</button></td></tr>";
+
+            PollTabbody.append(VoteItemCreation);
+
           });
-          function drawChart(ChartData, DivID) {
-              google.load("visualization", "1.0", { packages: ["bar"] });
-              google.charts.setOnLoadCallback(draw);
-              function draw() {
-                  var data = new google.visualization.DataTable();
-                  data.addColumn('string', 'Title');
-                  data.addColumn('string', 'PlaceOfInterest/ID');
-                  $.each(ChartData, function (key, value) {
-                      data.addRow([value.Title, value.PlaceOfInterest.ID]);
-                  });
-                  var chart = new google.visualization.Bar(document.getElementById(DivID));
-                  chart.draw(data, { allowHtml: true });
-              }
-          }*/
-  
-  
-/**------- rnds chart */
+          /**-------- Enabling Previous Vote and disabling remaining------ */
+          if (true) {
+            $('.VotePollbut').prop('disabled', true);
+            $("#" + SelectedBtnID).addClass("active").prop('disabled', false);
+          }
+        });
+
+
+        call.fail(function (jqXHR, textStatus, errorThrown) {
+          var response = JSON.parse(jqXHR.responseText);
+          var message = response ? response.error.message.value : textStatus;
+          alert("Call failed. Error: " + message);
+        });
+
+
+      }
+
+
+      /**--------- ends the HTML creation for Venues  --------------------*/
+
+      /**------- Chart Creation------------------------------------------------------ */
+      //ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingMembers')/items?$select=MemberName,ID,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID"
+      /* var dataURL = ParentSiteUrl + "/_api/Lists/getbyTitle('LokPollingMembers')/Items?$top=50&$select=Title,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID";
+       $.getJSON(dataURL, function (data) {
+           var dataFromSharepointList = data.value;
+ 
+           drawChart(dataFromSharepointList, "PieChart")
+       });
+       function drawChart(ChartData, DivID) {
+           google.load("visualization", "1.0", { packages: ["bar"] });
+           google.charts.setOnLoadCallback(draw);
+           function draw() {
+               var data = new google.visualization.DataTable();
+               data.addColumn('string', 'Title');
+               data.addColumn('string', 'PlaceOfInterest/ID');
+               $.each(ChartData, function (key, value) {
+                   data.addRow([value.Title, value.PlaceOfInterest.ID]);
+               });
+               var chart = new google.visualization.Bar(document.getElementById(DivID));
+               chart.draw(data, { allowHtml: true });
+           }
+       }*/
+
+
+      /**------- rnds chart */
 
     });
-    $(document).on("click", ".btn-primary" , function() {
-      SelectedBtnID=0;
+
+    /**----------- enable and disable by only voted button-------------  */
+    $(document).on("click", ".btn-primary", function () {
+      SelectedBtnID = 0;
       var clicked = $(this);
-      if(clicked.hasClass('active')){
+      if (clicked.hasClass('active')) {
         $('.VotePollbut').prop('disabled', false);
-      clicked.removeClass('active');
-      }else{
+        clicked.removeClass('active');
+      } else {
         $('.VotePollbut').prop('disabled', true);
         clicked.prop('disabled', false).addClass("active");
       }
-      
 
-      
+
+
     });
 
-    
-   
+
+
     /**------- Submit Vote clicked,.. list has to be updated------------- */
-    $(document).on("click", "#VoteSubmit" , function() {
+    $(document).on("click", "#VoteSubmit", function () {
       UpdateItem(SelectedBtnID);
     });
-    function UpdateItem(SelectedBtnID)
-    { 
-      $('#PollTabBody .active').each(function(){
+    function UpdateItem(SelectedBtnID) {
+      $('#PollTabBody .active').each(function () {
         /**-------------------------Id Of the active button iscollected------------  */
-        SelectedBtnID= $(this).attr('id'); 
-        if(isEmpty(SelectedBtnID) ){
+        SelectedBtnID = $(this).attr('id');
+        if (isEmpty(SelectedBtnID)) {
           alert("12")
-        }{
+        } {
           alert(SelectedBtnID);
         }
-        
-        
-    });
+
+
+      });
       /**----------- list updated----------------- */
-        pnp.sp.web.lists.getByTitle("LokPollingMembers").items.getById(CurrUserIdInList).update({
-         PlaceOfInterestId : SelectedBtnID   });
-        
+      pnp.sp.web.lists.getByTitle("LokPollingMembers").items.getById(CurrUserIdInList).update({
+        PlaceOfInterestId: SelectedBtnID
+      });
+
     }
-   
+
   }
- 
-  
- 
+
+
+
   /* ------------ getting user details
   alert(email);
 public getUserId(email: string): Promise<number> {
@@ -243,8 +266,8 @@ return result.data.Id;
 });
 } */
 
-  
-  
+
+
 
 
 
