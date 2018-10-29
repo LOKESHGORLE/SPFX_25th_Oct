d49d83e1a5360f80ecfb648b6e1860f234bf341a
LOKESHGORLE <43067941+LOKESHGORLE@users.noreply.github.com>
2018-10-29 18:31:47 +0530

    done everything
    has to add user if doesnt exists.

--- PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
+++ PollingApp/src/webparts/pollingApp/PollingAppWebPart.ts
@@ -12,8 +12,9 @@ import { SPComponentLoader } from '@microsoft/sp-loader';
 //import  * as $ from 'jquery';
 import 'jquery';
 require('bootstrap');
-//import  'charts';
-import Chart from 'chart.js';
+//import 'charts';
+import {GoogleCharts} from 'google-charts';
+//import Chart from 'chart.js';
 import styles from './PollingAppWebPart.module.scss';
 import * as strings from 'PollingAppWebPartStrings';
 
@@ -46,6 +47,9 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
         <button type='button center-block' id='VoteSubmit'class='VoteSubmitClass'>Submit Vote</button>
       </div>
   </div>
+  <div id="PieChart1">
+   
+  </div>
   <div id="PieChart">
     <canvas id="pie-chart" width="30%" height="30%"></canvas>
   </div>
@@ -57,7 +61,9 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
 
      `;
+     
     this.getReady();
+    //this.drawChart();
     /*-------------------to get the id of the user ///// failed ////--------  */
     var a;
     var b;
@@ -76,16 +82,47 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
     //})
 
   }
+
+
+
+
+
+
+
+  /*
+  private drawChart() {
+    alert("entering charts");
+    var data = Google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work', 8],
+    ['Eat', 2],
+    ['TV', 4],
+    ['Gym', 2],
+    ['Sleep', 8]
+  ]);
+  
+    // Optional; add a title and set the width and height of the chart
+    var options = {'title':'My Average Day', 'width':550, 'height':400};
+  
+    // Display the chart inside the <div> element with id="piechart"
+    var chart = new Google.visualization.PieChart(document.getElementById('PieChart1'));
+    chart.draw(data, options);
+  }*/
   /*------------------ calling the document ready and all the jquery-------------    */
   private getReady() {
     var ParentSiteUrl = this.context.pageContext.web.absoluteUrl;
     var SelectedBtnID;
+    var PreviousSelctedOptionID;
     var CurrUseremail = this.context.pageContext.user.email;
     var CurrUserIdInList;
+    var ArrayLocation =[];
+    var ArrayLocationVotes=[[]];
+    var PieChartDataLegends=[['Location','Votes'] ];
+    var PieChartData;
     //alert(CurrUseremail);
-
+    
     $(document).ready(function () {
-      /************************Pie Chart*****************/
+      /************************Pie Chart*****************
       new Chart(document.getElementById("pie-chart"), {
         type: 'pie',
         data: {
@@ -104,8 +141,13 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
         }
     });
 
- /************************Pie Chart*****************/
+ ***********************Pie Chart*****************/
       alert("enetred jquery");
+      //ArrayLocation[0]="arra element";
+     // alert (ArrayLocation[0]);
+     // ArrayLocationVotes[0]=["london",1];
+     // alert(ArrayLocationVotes[0][0]);
+
 
       /**------ verifying User in the list ------------------ */
 
@@ -127,12 +169,14 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
           /**---------- verifying if the user is present in the list or not------------- */
           if (value.MemberName == CurrUseremail) {
             CurrUserIdInList = value.ID;
-            SelectedBtnID = value.PlaceOfInterest.ID;
+            PreviousSelctedOptionID = value.PlaceOfInterest.ID;
+            alert(PreviousSelctedOptionID);
 
-            CreateVenueButtons(SelectedBtnID);//calling button creation function
+            CreateVenueButtons(PreviousSelctedOptionID);//calling button creation function
           }
-
+          
         });
+        
       });
       call.fail(function (jqXHR, textStatus, errorThrown) {
         var response = JSON.parse(jqXHR.responseText);
@@ -141,9 +185,9 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
       });
 
       /**------------ creating HTML element to show VENUE---------------------- */
-      function CreateVenueButtons(SelectedBtnID) {
+      function CreateVenueButtons(PreviousSelctedOptionID) {
         var callAssignDisplayItems = jQuery.ajax({
-          url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description",
+          url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description,VoteCount",
           type: "GET",
           dataType: "json",
           headers: {
@@ -157,20 +201,25 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
           var VoteItemCreation;
           var ItemClass = "";
           jQuery.each(data.d.results, function (index, value) {
-
-
+            ArrayLocation[index]=value.Title;
+            ArrayLocationVotes[index]=[value.Title,value.VoteCount];
+           
+      
             /**-------------------------creating HTML TAG----------------------------- */
             VoteItemCreation = "<tr><td>" + value.Description + "</td><td><button type='button' id='" + value.ID + "'class='btn btn-primary VotePollbut " + ItemClass + "'>" + value.Title + "</button></td></tr>";
 
             PollTabbody.append(VoteItemCreation);
-
+            
           });
+          GoogleCharts.load(drawChart);
+          
           /**-------- Enabling Previous Vote and disabling remaining------ */
-          if (true) {
+          if (PreviousSelctedOptionID!=0) {
             $('.VotePollbut').prop('disabled', true);
-            $("#" + SelectedBtnID).addClass("active").prop('disabled', false);
+            $("#" + PreviousSelctedOptionID).addClass("active").prop('disabled', false);
           }
-        });
+        }
+        );
 
 
         call.fail(function (jqXHR, textStatus, errorThrown) {
@@ -181,9 +230,71 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
 
       }
-
-
       /**--------- ends the HTML creation for Venues  --------------------*/
+      
+ function PieChartDraw(){
+
+  var callAssignDisplayItems = jQuery.ajax({
+    url: ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingVenues')/items?$select=Title,ID,Description,VoteCount",
+    type: "GET",
+    dataType: "json",
+    headers: {
+      Accept: "application/json;odata=verbose"
+    }
+  });
+
+  var call = jQuery.when(callAssignDisplayItems);
+  call.done(function (data, textStatus, jqXHR) {
+    
+    jQuery.each(data.d.results, function (index, value) {
+      
+     
+      
+    
+    });
+    /**-------- take data for pie chart      also cal pie cghart------ */
+    
+  }
+  );
+
+
+  call.fail(function (jqXHR, textStatus, errorThrown) {
+    var response = JSON.parse(jqXHR.responseText);
+    var message = response ? response.error.message.value : textStatus;
+    alert("Call failed. Error: " + message);
+  });
+
+
+ }
+
+
+
+
+function drawChart() {
+  
+    PieChartData=PieChartDataLegends;
+    ArrayLocationVotes.forEach(element => {
+      PieChartData.push(element);
+    }); 
+    // Standard google charts functionality is available as GoogleCharts.api after load
+    const data = GoogleCharts.api.visualization.arrayToDataTable(PieChartData
+        // [ 
+        //  ['Chart thing', 'Chart amount'],
+        //  ['Lorem ipsum', 60],
+        //  ['Dolor sit', 22],
+        //  ['Sit amet', 18]
+        // ]
+    );
+    const pie_1_chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('PieChart1'));
+    pie_1_chart.draw(data);
+    
+}
+      
+
+
+
+
+
 
       /**------- Chart Creation------------------------------------------------------ */
       //ParentSiteUrl + "/_api/web/lists/getByTitle('LokPollingMembers')/items?$select=MemberName,ID,PlaceOfInterest/ID&$expand=PlaceOfInterest/ID"
@@ -233,9 +344,32 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
     /**------- Submit Vote clicked,.. list has to be updated------------- */
     $(document).on("click", "#VoteSubmit", function () {
-      UpdateItem(SelectedBtnID);
+      UpdateLists();
+
+      GoogleCharts.load(drawChart1);
+ 
+     function drawChart1() {
+      PieChartData=[[]];
+      var PieChartDataLegends=[['Location','Votes'] ];
+      PieChartData=PieChartDataLegends;
+      ArrayLocationVotes.forEach(element => {
+        PieChartData.push(element);
+      }); 
+      
+         // Standard google charts functionality is available as GoogleCharts.api after load
+         const data = GoogleCharts.api.visualization.arrayToDataTable(PieChartData);
+             //[ ['Chart thing', 'Chart amount'],
+             // ['Lorem ipsum', 60],
+             // ['Dolor sit', 22],
+             // ['Sit amet', 18]
+             //]
+         
+         const pie_1_chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('PieChart1'));
+         pie_1_chart.draw(data);
+     }
+     PieChartData=[[]];
     });
-    function UpdateItem(SelectedBtnID) {
+    function UpdateLists() {
       $('#PollTabBody .active').each(function () {
         /**-------------------------Id Of the active button iscollected------------  */
         SelectedBtnID = $(this).attr('id');
@@ -247,11 +381,42 @@ export default class PollingAppWebPart extends BaseClientSideWebPart<IPollingApp
 
 
       });
-      /**----------- list updated----------------- */
-      pnp.sp.web.lists.getByTitle("LokPollingMembers").items.getById(CurrUserIdInList).update({
-        PlaceOfInterestId: SelectedBtnID
-      });
+            if(SelectedBtnID!=PreviousSelctedOptionID){
+            /* increase the vote count.... write function to post in venues votecount*/ 
+            var OldLocationIndex=PreviousSelctedOptionID-1;
+            var OldVoteCount=ArrayLocationVotes[OldLocationIndex][1]-1;
+            
 
+            var NewLocationIndex=SelectedBtnID-1;
+            var NewVoteCount=ArrayLocationVotes[NewLocationIndex][1]+1;
+
+            /**----------- Members list updated----------------- */
+            pnp.sp.web.lists.getByTitle("LokPollingVenues").items.getById(PreviousSelctedOptionID).update({
+              VoteCount: OldVoteCount
+              });
+
+
+            pnp.sp.web.lists.getByTitle("LokPollingVenues").items.getById(SelectedBtnID).update({
+            VoteCount: NewVoteCount
+            });
+            /**----------- Members list updated----------------- */
+            pnp.sp.web.lists.getByTitle("LokPollingMembers").items.getById(CurrUserIdInList).update({
+              PlaceOfInterestId: SelectedBtnID
+            });
+
+            
+            
+
+           
+ 
+            }
+     /********* sending two votes, the newly selected button must be Prev Selected for second transaction */
+     ArrayLocationVotes[OldLocationIndex][1]=OldVoteCount;      
+     ArrayLocationVotes[NewLocationIndex][1]=NewVoteCount;
+
+     PreviousSelctedOptionID=SelectedBtnID;
+
+      
     }
 
   }
